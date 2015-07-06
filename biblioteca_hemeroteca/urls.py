b4d170d70b9b65a4059d05438f9b038fff58b8fb
hectorz11 <hector.rz11@gmail.com>
2015-07-06 09:59:29 -0500

    staticfiles add and authenticate and logout fails

--- biblioteca_hemeroteca/urls.py
+++ biblioteca_hemeroteca/urls.py
@@ -6,6 +6,8 @@ import settings
 urlpatterns = [
 
 	url(r'^', include('apps.urls', namespace='apps')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT}),
+	url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',
+    	{'document_root':settings.MEDIA_ROOT}),
 ]
